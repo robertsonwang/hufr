21c7ed9ed0ca03fe986fb4a7bb8cd09c8121b311
robertsonwang <rob@wearethorn.org>
2024-01-14 19:45:49 -0800

    moved tests into integration and unit folders

---
+++ tests/integration/test_redact.py
@@ -0,0 +1,19 @@
+from hufr.redact import redact_text
+
+def test_redact_text_default_model():
+    """Test the redact_text loads a default model and default redaction scheme"""
+    # Test input text
+    input_text = "This is a test. My name is John"
+
+    # Expected redacted text
+    expected_redacted_text = "This is a test. My name is <PERSON>."
+
+    # Expected model predictions
+    expected_return_value = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'PER']
+
+    # Call the redact_text function
+    result, pred= redact_text(text=input_text, return_preds=True)
+
+    # Assert the redacted text is as expected
+    assert result == expected_redacted_text
+    assert pred == expected_return_value
